407c43a069a83ca0665660d7589032d95712991b
Alexey Suchkov <alexey.suchkov@gmail.com>
2020-07-30 18:36:29 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,120 @@
+import psycopg2
+from data import students_list, course_list
+
+
+def create_db():
+    with psycopg2.connect(database='psql', user='psql', password='12345', host='pg.codecontrol.ru', port=59432) as conn:
+        tables = {'course': "CREATE TABLE course(id integer NOT NULL, name varchar(100) NOT NULL);",
+                  'student': "CREATE TABLE student(id integer NOT NULL, name varchar(100) NOT NULL, "
+                             "gpa numeric(10, 2), birth timestamp with time zone);",
+                  'student_course': "CREATE TABLE student_course(course_id integer NOT NULL, "
+                                    "student_id integer NOT NULL);"
+                  }
+        with conn.cursor() as curs:
+            for name, query in tables.items():
+                curs.execute("select exists(select * from information_schema.tables where table_name='%s');" % name)
+                result = curs.fetchone()[0]
+                if not result:
+                    curs.execute("%s" % query)
+
+
+def add_course(course_list):
+    with psycopg2.connect(database='psql', user='psql', password='12345', host='pg.codecontrol.ru', port=59432) as conn:
+        with conn.cursor() as curs:
+            for item in course_list:
+                for dict_key, value in item.items():
+                    curs.execute("select id from course where id=%s" % dict_key)
+                    result_query = curs.fetchone()
+                    if result_query is None:
+                        curs.execute('insert into course(id, name) values(%s, %s)', (dict_key, value))
+
+
+def get_students(course_id):
+    with psycopg2.connect(database='psql', user='psql', password='12345', host='pg.codecontrol.ru', port=59432) as conn:
+        with conn.cursor() as curs:
+            curs.execute("select student.name, course.name from student join student_course on "
+                         "student.id = student_course.student_id join course on "
+                         "course.id = student_course.course_id where course.id = %s" % course_id)
+            data = curs.fetchall()
+            if data:
+                for row in data:
+                    print(f"На курсе '{row[1]}' обучается {row[0]}")
+            else:
+                print(f"На данном курсе никто не обучается!")
+
+
+def add_students(course_id, students):
+    with psycopg2.connect(database='psql', user='psql', password='12345', host='pg.codecontrol.ru', port=59432) as conn:
+        with conn.cursor() as curs:
+            for item in students:
+                for dict_key in item.keys():
+                    curs.execute("select id from student where id=%s" % item[dict_key]['id'])
+                    result_query = curs.fetchone()
+                    if result_query is None:
+                        curs.execute('insert into student(id, name, gpa, birth) values(%s, %s, %s, %s)',
+                                     (item[dict_key]['id'], item[dict_key]['name'], item[dict_key]['gpa'],
+                                      item[dict_key]['birth']))
+                    else:
+                        print(f"Студент {item[dict_key]['name']} уже есть в базе")
+                    curs.execute('select * from student_course where course_id=%s and student_id=%s;',
+                                 (course_id, item[dict_key]['id']))
+                    if not curs.fetchone():
+                        curs.execute('insert into student_course(course_id, student_id) values(%s, %s)',
+                                     (course_id, item[dict_key]['id']))
+                    else:
+                        print(f"Студент {item[dict_key]['name']} уже записан на курс")
+
+
+def add_student(student):
+    with psycopg2.connect(database='psql', user='psql', password='12345', host='pg.codecontrol.ru', port=59432) as conn:
+        with conn.cursor() as curs:
+            for item in student:
+                for dict_key in item.keys():
+                    curs.execute("select id from student where id=%s" % item[dict_key]['id'])
+                    result_query = curs.fetchone()
+                    if result_query is None:
+                        curs.execute('insert into student(id, name, gpa, birth) values(%s, %s, %s, %s)',
+                                     (item[dict_key]['id'], item[dict_key]['name'], item[dict_key]['gpa'],
+                                      item[dict_key]['birth']))
+                    else:
+                        print(f"Студент {item[dict_key]['name']} уже есть в базе")
+
+
+def get_student(student_id):
+    with psycopg2.connect(database='psql', user='psql', password='12345', host='pg.codecontrol.ru', port=59432) as conn:
+        with conn.cursor() as curs:
+            curs.execute("select student.name, student.gpa, student.birth, course.name from student join "
+                         "student_course on student.id=student_course.student_id join course on "
+                         "course.id=student_course.course_id where student_course.student_id=%s" % student_id)
+            result_query = curs.fetchall()
+            if len(result_query) == 1:
+                print('Имя:', result_query[0][0])
+                print('Средняя оценка:', result_query[0][1])
+                print('Дата рождения:', result_query[0][2])
+                print('Курсы студента:', result_query[0][3])
+            elif len(result_query) > 1:
+                print('Имя:', result_query[0][0])
+                print('Средняя оценка:', result_query[0][1])
+                print('Дата рождения:', result_query[0][2])
+                courses = ""
+                for data in result_query:
+                    courses += data[3] + ","
+                print("Курсы студента:", courses.rstrip(","))
+            else:
+                print("Такого студента нет!")
+
+            return curs.fetchall()
+
+
+if __name__ == "__main__":
+    create_db()
+
+    add_student(students_list)
+
+    add_students(2, students_list)
+
+    add_course(course_list)
+
+    get_student(7)
+
+    get_students(2)
---
+++ data.py
@@ -0,0 +1,43 @@
+course_list = [
+    {'1': 'Python'},
+    {'2': 'Data Science'},
+    {'3': 'JS'},
+]
+
+students_list = [
+    {'Student 1': {'id': 1,
+                   'name': 'Ivan Ivanov',
+                   'gpa': 5.2,
+                   'birth': '1980-01-01'}
+     },
+    {'Student 2': {'id': 2,
+                   'name': 'Petr Petrov',
+                   'gpa': 4.2,
+                   'birth': '1981-01-01'}
+     },
+    {'Student 3': {'id': 3,
+                   'name': 'Mikhail Mikhailov',
+                   'gpa': 2.8,
+                   'birth': '1982-01-01'}
+     },
+    {'Student 4': {'id': 4,
+                   'name': 'Dmitry Dmitriev',
+                   'gpa': 4.8,
+                   'birth': '1983-01-01'}
+     },
+    {'Student 5': {'id': 5,
+                   'name': 'Alexey Alexeev',
+                   'gpa': 4.8,
+                   'birth': '1984-01-01'}
+     },
+    {'Student 6': {'id': 6,
+                   'name': 'Egor Egorov',
+                   'gpa': 1.8,
+                   'birth': '1985-01-01'}
+     },
+    {'Student 7': {'id': 7,
+                   'name': 'Vladimir Vladimirov',
+                   'gpa': 5.2,
+                   'birth': '1986-01-01'}
+     },
+]
